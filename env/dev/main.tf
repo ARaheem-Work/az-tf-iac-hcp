a683cd57f176ee051e54671009495b12cf473d3a
ARAHEEM <araheem.work@outlook.com>
2026-06-26 00:39:01 -0700

    uncomment resource group module and import statements in Terraform configuration

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -1,6 +1,6 @@
-# module "az_resource_group" {
-#   source = "./dev-modules/az-rg"
-# }
+module "az_resource_group" {
+  source = "./dev-modules/az-rg"
+}
 module "az_storage_account" {
   source = "./dev-modules/az-storage-account"
 }
