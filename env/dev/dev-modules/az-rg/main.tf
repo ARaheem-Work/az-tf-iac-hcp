2648b203ca6a0a1cc982ae2c80b09dd1cdb0b8b8
ARAHEEM <araheem.work@outlook.com>
2026-06-26 12:07:33 -0700

    uncomment resource group configuration in dev module main.tf

--- env/dev/dev-modules/az-rg/main.tf
+++ env/dev/dev-modules/az-rg/main.tf
@@ -1,17 +1,12 @@
-# resource "azurerm_resource_group" "res-0" {
-#   location = "westus3"
-#   name     = "rg-06252026a"
-#   tags = {
-#     environment = "dev"
-#     project     = "az-tf-iac-hcp"
-#     owner       = "platform-team"
-#     managed_by  = "terraform"
-#   }
-# }
-
-removed {
-  from = azurerm_resource_group.res-0
-
+resource "azurerm_resource_group" "res-0" {
+  location = "westus3"
+  name     = "rg-06252026a"
+  tags = {
+    environment = "dev"
+    project     = "az-tf-iac-hcp"
+    owner       = "platform-team"
+    managed_by  = "terraform"
+  }
   lifecycle {
     destroy = false
   }
