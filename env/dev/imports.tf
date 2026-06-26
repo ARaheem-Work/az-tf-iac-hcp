d6ea1be43834d1d9695bc45fac113da2ea669ee8
ARAHEEM <araheem.work@outlook.com>
2026-06-26 00:35:50 -0700

    comment out resource group module and import statements in Terraform configuration

--- env/dev/imports.tf
+++ env/dev/imports.tf
@@ -1,7 +1,7 @@
-import {
-  id = "/subscriptions/fc173b43-c76c-4e81-95b1-15cfc37abdda/resourceGroups/rg-06252026a"
-  to = module.az_resource_group.azurerm_resource_group.res-0
-}
+# import {
+#   id = "/subscriptions/fc173b43-c76c-4e81-95b1-15cfc37abdda/resourceGroups/rg-06252026a"
+#   to = module.az_resource_group.azurerm_resource_group.res-0
+# }
 
 
 import {
