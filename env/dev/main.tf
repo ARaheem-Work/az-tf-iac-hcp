bd237b75ebcbcb52bfc15ce9bd89792a5f3b4b9d
ARAHEEM <araheem.work@outlook.com>
2026-06-26 11:32:15 -0700

    fix: update reference in removed block to use direct resource name

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -7,7 +7,7 @@ module "az_storage_account" {
 
 
 removed {
-  from = module.az_resource_group.azurerm_resource_group.res-0
+  from = azurerm_resource_group.res-0
 
   lifecycle {
     destroy = false
