b2e503deaea83e27476db1c529a96f8d67b8530a
ARAHEEM <araheem.work@outlook.com>
2026-06-26 11:28:40 -0700

    uncomment Terraform provider configuration in versions.tf and resource group module in main.tf

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -1,15 +1,15 @@
-module "az_resource_group" {
-  source = "./dev-modules/az-rg"
-}
+# module "az_resource_group" {
+#   source = "./dev-modules/az-rg"
+# }
 module "az_storage_account" {
   source = "./dev-modules/az-storage-account"
 }
 
 
-# removed {
-#   from = module.az_resource_group.azurerm_resource_group.res-0
+removed {
+  from = module.az_resource_group.azurerm_resource_group.res-0
 
-#   lifecycle {
-#     destroy = false
-#   }
-# }
+  lifecycle {
+    destroy = false
+  }
+}
