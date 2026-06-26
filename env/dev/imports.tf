8a6d7654a6ea65e2df6afc2760cdd5ce39610a84
ARAHEEM <araheem.work@outlook.com>
2026-06-26 00:11:05 -0700

    uncomment import statements in Terraform configuration for resource group and storage account

--- env/dev/imports.tf
+++ env/dev/imports.tf
@@ -1,11 +1,11 @@
-# import {
-#   id = "/subscriptions/fc173b43-c76c-4e81-95b1-15cfc37abdda/resourceGroups/rg-06252026a"
-#   to = module.az_resource_group.azurerm_resource_group.res-0
-# }
+import {
+  id = "/subscriptions/fc173b43-c76c-4e81-95b1-15cfc37abdda/resourceGroups/rg-06252026a"
+  to = module.az_resource_group.azurerm_resource_group.res-0
+}
 
 
-# import {
-#   id = "/subscriptions/fc173b43-c76c-4e81-95b1-15cfc37abdda/resourceGroups/rg-06252026a/providers/Microsoft.Storage/storageAccounts/azst06252026a"
-#   to = module.az_storage_account.azurerm_storage_account.res-0
-# }
+import {
+  id = "/subscriptions/fc173b43-c76c-4e81-95b1-15cfc37abdda/resourceGroups/rg-06252026a/providers/Microsoft.Storage/storageAccounts/azst06252026a"
+  to = module.az_storage_account.azurerm_storage_account.res-0
+}
 
